8cf4f18a119d166e8756d07bb48c4acb5da02a59
aeropriest <ashok.jaiswal@gmail.com>
2021-09-01 16:30:20 +0800

    Added project boiler plate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HomePage from './pages/homepage.component';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HomePage></HomePage>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,8 @@
+
+const HomePage = () => {
+    return(
+        <h1>Big Background Shoule Be Here</h1>
+    )
+}
+
+export default HomePage
