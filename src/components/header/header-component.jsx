fd60567ca10670367a589ac61621b7f99dfcae43
aeropriest <ashok.jaiswal@gmail.com>
2021-09-11 10:22:53 +0800

    Completed metamask integration

--- src/components/header/header-component.jsx
+++ src/components/header/header-component.jsx
@@ -4,9 +4,55 @@ import { ReactComponent as DropMenu } from './../../assets/images/drop-down.svg'
 import { ReactComponent as MainMenu } from './../../assets/images/main-menu.svg'
 import { ReactComponent as UserIcon } from './../../assets/images/user-icon.svg'
 import SearchDialog from '../search-dialog/search-dialog.component'
-
+import Web3 from "web3";
 
 import './header-component.styles.css'
+
+const connectWallet = async (data) => {
+    try {
+        // Get network provider and web3 instance.
+        //const web3 = await getWeb3();
+        let web3
+        if( window.ethereum ){
+          web3 = new Web3(window.ethereum)
+          await window.ethereum.enable()        
+        } else if( window.web3 ){
+          web3 = new Web3(window.web3.currentProvider)
+        }      
+  
+        // Use web3 to get the user's accounts.
+        const accounts = await web3.eth.getAccounts();
+  
+  
+        // Get the contract instance.
+        // const networkId = await web3.eth.net.getId();
+        // const deployedNetwork = SimpleStorageContract.networks[networkId];
+        // const instance = new web3.eth.Contract(
+        //   SimpleStorageContract.abi,
+        //   deployedNetwork && deployedNetwork.address,
+        // );
+  
+        console.log(accounts)
+        // Set web3, accounts, and contract to the state, and then proceed with an
+        // example of interacting with the contract's methods.
+        //this.setState({ web3, accounts, contract: instance }, this.runExample);
+      } catch (error) {
+        // Catch any errors for any of the above operations.
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`,
+        );
+        console.error(error);
+      }
+
+    // let promise = new Promise((resolve, reject) => {
+    //     setTimeout(() => resolve("I am a done promise!"), 3000)
+    // });
+
+    // let result = await promise
+
+    // alert(result);
+}
+
 const Header = () => {
     const [open, setOpen] = useState(false)
     return(
@@ -18,14 +64,16 @@ const Header = () => {
                     onClick={() => { setOpen(true) }}
                     />
                     <DropMenu className='option' style={{height:'30px'}}/>
-                    <UserIcon className='option'/>
+                    <UserIcon className='option'
+                    onClick={() => { 
+                        connectWallet("Example")
+                     }}
+                    />
                 </div>
                 <SearchDialog
                 open={open}
                 setOpen={setOpen}
-                >
-            </SearchDialog>
-
+                />
             </div>
     )
 }
